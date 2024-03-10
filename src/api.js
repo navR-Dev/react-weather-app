f36ce1f9ca688434e1796c18f316cf416ff5f3a3
naveenRDev <114591918+naveenRDev@users.noreply.github.com>
2024-03-10 15:26:30 +0530

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b92392df80msh3403fcf1f5c9066p1ee735jsne10e47507263",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const WEATHER_API_KEY = "4239d3cc5d5b9cc5961c01144d412182";
